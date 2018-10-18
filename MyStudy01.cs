887717f1759caa2bf69dde94747ab2c623c0e6ff
wmadwand <wmadwand@gmail.com>
2018-10-18 11:59:21 +0300

    MyStudy01

---
+++ MyStudy01.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace TemplateMethod
+{
+    public abstract class Education
+    {
+        public void Learn()
+        {
+            Enter();
+            Study();
+            PassExams();
+            GetDocument();
+        }
+
+        protected abstract void Enter();
+        protected abstract void Study();
+
+        protected virtual void PassExams()
+        {
+            Console.WriteLine("PassExams");
+        }
+
+        protected abstract void GetDocument();
+    }
+
+    public class School : Education
+    {
+        protected override void Enter()
+        {
+            Console.WriteLine("School Enter with no exams");
+        }
+
+        protected override void Study()
+        {
+            Console.WriteLine("School Study: attend lessons");
+        }
+
+        protected override void GetDocument()
+        {
+            Console.WriteLine("School GetDocument: basic education doc");
+        }
+    }
+
+    public class University : Education
+    {
+        public new void Learn()
+        {
+            base.Learn();
+            //Console.WriteLine("University: We don't need no education! F*ck them all!");
+        }
+
+        protected override void Enter()
+        {
+            Console.WriteLine("University Enter with initial exams");
+        }
+
+        protected override void Study()
+        {
+            Console.WriteLine("University Study: attend lectures");
+            Console.WriteLine("University Study: do practise");
+        }
+
+        protected override void PassExams()
+        {
+            base.PassExams();
+            Console.WriteLine("University PassExams: use lot of hacks along");
+        }
+
+        protected override void GetDocument()
+        {
+            Console.WriteLine("University Study: get your diploma and get out of here");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School school = new School();
+            University university = new University();
+
+            school.Learn();
+            university.Learn();
+
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 
-namespace TemplateMethod
+namespace TemplateMethod1
 {
-    class Program
+    abstract class Education
     {
-        static void Main(string[] args)
+        public void Learn()
         {
-            Console.WriteLine("Hello World!");
+            Enter();
+            Study();
+            PassExams();
+            GetDocument();
+        }
+
+        public abstract void Enter();
+
+        public abstract void Study();
+
+        public virtual void PassExams()
+        {
+            Console.WriteLine("Сдаем выпускные экзамены");
+        }
+
+        public abstract void GetDocument();
+    }
+
+    class School : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Идем в первый класс");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Посещаем уроки, делаем домашние задания");
+        }
+
+        public override void GetDocument()
+        {
+            Console.WriteLine("Получаем аттестат о среднем образовании");
+        }
+    }
+
+    class University : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Сдаем вступительные экзамены и поступаем в ВУЗ");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Посещаем лекции");
+            Console.WriteLine("Проходим практику");
+        }
+
+        public override void PassExams()
+        {
+            Console.WriteLine("Сдаем экзамен по специальности");
+        }
+
+        public override void GetDocument()
+        {
+            Console.WriteLine("Получаем диплом о высшем образовании");
+        }
+    }
+
+    class Program1
+    {
+        static void Main1(string[] args)
+        {
+            School school = new School();
+            University university = new University();
+
+            school.Learn();
+            university.Learn();
+
+            Console.Read();
         }
     }
 }
